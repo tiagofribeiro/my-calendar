4337b3fc32fba1aa844feeac77c73d3c60130860
tiagofribeiro <tiagofr98@gmail.com>
2023-04-28 18:24:43 -0300

    :fountain_pen: wip: creating models
    created Task model; TaskType enum

---
+++ WPFCalendar/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFCalendar.Models.enums;
+
+namespace WPFCalendar.Models
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public TaskType Type { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime? Deadline { get; set; }
+
+    }
+}
---
+++ WPFCalendar/Models/enums/TaskType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalendar.Models.enums
+{
+    public enum TaskType
+    {
+        Pessoal,
+        Profissional,
+        Saude,
+        Casa,
+        Pet,
+        Gov
+    }
+}
