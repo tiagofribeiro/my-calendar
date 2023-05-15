85f475a26611e6e00e986c2cfeb2f62678d21c95
tiagofribeiro <tiagofr98@gmail.com>
2023-05-15 18:32:08 -0300

    :fountain_pen: wip: creating components
    + card development

--- WPFCalendar/MainWindow.xaml.cs
+++ WPFCalendar/MainWindow.xaml.cs
@@ -28,12 +28,14 @@ namespace WPFCalendar
 
         private void AddEvent(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("Adicionar evento!");
+            Debug.WriteLine(sender);
         }
 
         private void AddTask(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("Adicionar tarefa!");
+            Debug.WriteLine(sender);
         }
     }
 }
